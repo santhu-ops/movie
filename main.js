3a6a7c4c7bce48703887008d1182f0f964a24b26
santhu-ops <kumarsanthosh26007@gmail.com>
2026-06-05 08:06:41 +0530

    ROUTE STURCTURE

--- main.js
+++ main.js
@@ -9,6 +9,45 @@ app.get('/',(req,res)=>{
 
 })
 
+// CRUD functionality movies
+
+//for reading the movies
+
+// R-READING 
+app.get("/movies",()=>{
+
+})
+
+
+//For creating movies
+
+
+// C-CREATING
+app,post('/movies',()=>{
+
+})
+
+// U -UPDATING
+app.put('/movies/:id',()=>{
+
+})
+
+
+// D- DELETE
+app.delete('/movie/:id',()=>{
+
+})
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT,()=>{
     console.log("server is runnning");
